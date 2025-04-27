9bf631cbd015ff73527df8fc89665b12a7073653
adriangit25 <adrenher2468@gmail.com>
2025-04-26 23:52:49 -0500

    Resuelto conflicto con el repositorio remoto

--- obj/Debug/net9.0/LoginBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/LoginBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb3c660e29d69ce2c38b1fe1d98be88c8279f81")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
