13db56c0a64c0134925d2e0dabd8d593d860d030
adriangit25 <adrenher2468@gmail.com>
2025-04-27 01:56:07 -0500

    Eliminar migraciones y modelos antiguos

--- obj/Debug/net9.0/LoginBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/LoginBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b3b829ba6de917eeebbd3e40bc1b09795a394d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59f939bc7c062d6984e8223b1083bcf6f581de5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
